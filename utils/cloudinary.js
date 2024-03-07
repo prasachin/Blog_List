1f3eda45a9186ee5dd0942a251286a97ded5e853
sachin <sachinp@iitbhilai.ac.in>
2024-03-07 16:03:09 +0530

    up

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,29 +1,32 @@
-const v2 = require("cloudinary");
+const cloudinary = require("cloudinary").v2;
 const fs = require("fs");
 
-v2.config({
+cloudinary.config({
   cloud_name: process.env.CLOUDINARY_API_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const UploadOncloudinary = async (localfilepath) => {
+const UploadOncloudinary = async (localFilePath) => {
   try {
-    if (!localfilepath) return null;
-    const response = await v2.uploader.upload(localfilepath, {
+    if (!localFilePath) throw new Error("Local file path is missing");
+
+    const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     });
-    console.log("file uploaded successfully ", response.url);
-    fs.unlink(localfilepath, (err) => {
-      if (err) {
-        console.error("Error deleting file:", err);
-        return;
-      }
-      console.log("Local file deleted successfully");
-    });
+
+    console.log("File uploaded successfully:", response.url);
+
+    await fs.promises.unlink(localFilePath);
+    console.log("Local file deleted successfully");
+
     return response;
   } catch (error) {
-    fs.unlinkSync(localfilepath);
+    console.error(
+      "Error uploading to Cloudinary or deleting local file:",
+      error
+    );
+    throw error;
   }
 };
 
